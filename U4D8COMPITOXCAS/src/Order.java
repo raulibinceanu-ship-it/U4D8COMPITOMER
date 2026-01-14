fb64ea58fcbc133f23ee51a13c7dc00675eabd88
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2026-01-14 14:46:02 +0100

    product, customer and order

---
+++ U4D8COMPITOXCAS/src/Order.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+
+    private Long id;
+    private String status;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Long id, String status, LocalDate orderDate, LocalDate deliveryDate,
+                 List<Product> products, Customer customer) {
+        this.id = id;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+}
---
+++ U4D8COMPITOXCAS/src/Product.java
@@ -0,0 +1,34 @@
+public class Product {
+    private Long id;
+    private String name;
+    private String category;
+    private Double price;
+
+    public Product(Long id, String name, String category, Double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + category + " - " + price + "€";
+    }
+}
