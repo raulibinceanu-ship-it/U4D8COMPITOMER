fb0b800eb63e46ec38c1e06c4b31df0c13f0ebc6
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2026-01-15 10:38:43 +0100

    l

---
+++ untitled1122/src/test/java/exempleraulibin/ApplicationTest.java
@@ -0,0 +1,13 @@
+package exempleraulibin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
